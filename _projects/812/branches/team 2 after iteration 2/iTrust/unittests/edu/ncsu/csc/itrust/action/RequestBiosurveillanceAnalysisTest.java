f31c178262552fef200ca9eda9454440ace65281
yang161 <yang161@027de155-b6e4-472d-81b2-d78c0521551b>
2014-11-14 04:21:56 +0000

    Team 2 : Got Influenza Unit Test working :)

--- _projects/812/branches/team 2 after iteration 2/iTrust/unittests/edu/ncsu/csc/itrust/action/RequestBiosurveillanceAnalysisTest.java
+++ _projects/812/branches/team 2 after iteration 2/iTrust/unittests/edu/ncsu/csc/itrust/action/RequestBiosurveillanceAnalysisTest.java
@@ -5,36 +5,33 @@ import edu.ncsu.csc.itrust.dao.DAOFactory;
 import edu.ncsu.csc.itrust.dao.mysql.OfficeVisitDAO;
 import edu.ncsu.csc.itrust.dao.mysql.PatientDAO;
 import edu.ncsu.csc.itrust.datagenerators.TestDataGenerator;
+import edu.ncsu.csc.itrust.testutils.TestDAOFactory;
 import junit.framework.TestCase;
 
 public class RequestBiosurveillanceAnalysisTest extends TestCase{
 	
 
-	private TestDataGenerator gen;
-	private BioSurveillanceBean bioBean;
+	private TestDataGenerator gen = new TestDataGenerator();
+	//private BioSurveillanceBean bioBean;
 	private  RequestBioSurveillanceAnalysisAction action;
 	
-	@Override
 	protected void setUp() throws Exception {
 		gen = new TestDataGenerator();
+		action = new RequestBioSurveillanceAnalysisAction(TestDAOFactory.getTestInstance());
 		gen.clearAllTables();
 		gen.standardData();
-		action = new RequestBioSurveillanceAnalysisAction();
-
+		
 	}
 	
 	public void testInfluenza() throws Exception {
 		gen.addThreePatiensWithSameZipCode();
-	
-	gen.addOfficeVisitsWithInfluenza();
-	
-	bioBean = new BioSurveillanceBean("487.00", "61820",
-			"01/14/2005");
-	assertTrue(action.requestBioAnalysis(bioBean));
-	
-	
-	
-	
+
+		gen.addOfficeVisitsWithInfluenza();
+
+		BioSurveillanceBean bioBean = new BioSurveillanceBean("487.00", "61820", "01/14/2005");
+		System.out.println("bioBean " + action.requestBioAnalysis(bioBean));
+		assertTrue(action.requestBioAnalysis(bioBean));
+
 	}
 
 }
