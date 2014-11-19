a23f1b21c343e1ef870041eecae01116b1c3aae2
pokapra2 <pokapra2@027de155-b6e4-472d-81b2-d78c0521551b>
2014-11-19 02:37:07 +0000

    Team 2 : finish UC 14

---
+++ _projects/812/tags/finishUC14/iTrust/src/edu/ncsu/csc/itrust/beans/BioSurveillanceBean.java
@@ -0,0 +1,127 @@
+package edu.ncsu.csc.itrust.beans;
+/**
+ * 
+ * Store information about biosurveillance
+ *
+ */
+
+public class BioSurveillanceBean {
+
+	String diagnosisCode;
+	String zipCode;
+	String date;
+	String threshold;
+	/**
+	 * Constructor for with threshold
+	 * @param diagnosisCode
+	 * @param zipCode
+	 * @param date
+	 */
+
+
+	public BioSurveillanceBean(String diagnosisCode, String zipCode,
+			String date, String threshold) {
+		super();
+		this.diagnosisCode = diagnosisCode;
+		this.zipCode = zipCode;
+		this.date = date;
+		this.threshold = threshold;
+	}
+
+	/**
+	 * Constructor for without threshold
+	 * @param diagnosisCode
+	 * @param zipCode
+	 * @param date
+	 */
+	public BioSurveillanceBean(String diagnosisCode, String zipCode,
+			String date) {
+
+		this.diagnosisCode = diagnosisCode;
+		this.zipCode = zipCode;
+		this.date = date;
+		this.threshold = "0";
+	}
+	
+	/**
+	 * 
+	 * @return the diagnosisCode
+	 */
+	
+	public String getDiagnosisCode() {
+		return diagnosisCode;
+	}
+	/**
+	 * 
+	 * @param diagnosisCode
+	 */
+	public void setDiagnosisCode(String diagnosisCode) {
+		this.diagnosisCode = diagnosisCode;
+	}
+	
+	public boolean isMalariaDiagCode(String diagnosisCode) {
+		String[] diagCodeInteger = diagnosisCode.split("\\.");
+		if(diagCodeInteger[0].equals("84") || diagCodeInteger[0].equals("084")) {
+			return true;
+		}
+		else{
+			return false;
+		}
+	}
+	
+	public boolean isInfluenzaDiagCode(String diagnosisCode) {
+		String[] diagCodeInteger = diagnosisCode.split("\\.");
+		if(diagCodeInteger[0].equals("487")) {
+			return true;
+		}
+		else{
+			return false;
+		}
+	}
+	
+	/**
+	 * 
+	 * @return the zipCode
+	 */
+	public String getZipCode() {
+		return zipCode;
+	}
+	/**
+	 * 
+	 * @param zipCode
+	 */
+	public void setZipCode(String zipCode) {
+		this.zipCode = zipCode;
+	}
+	/**
+	 * 
+	 * @return the date
+	 */
+	public String getDate() {
+		return date;
+	}
+	/**
+	 * 
+	 * @param date
+	 */
+	public void setDate(String date) {
+		this.date = date;
+	}
+	/**
+	 * 
+	 * @return the threshold
+	 */
+	public String getThreshold() {
+		return threshold;
+	}
+
+	/**
+	 * 
+	 * @param threshold
+	 */
+	public void setThreshold(String threshold) {
+		this.threshold = threshold;
+	}
+	
+
+}
